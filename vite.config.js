7903e8fdd894020b81173450f83533a6d8bf975f
Diego Fornalha <diegofornalha@gmail.com>
2023-10-15 02:37:48 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+const isHMRHostDefined = process.env.HMR_HOST;
+
+const hmrHost = isHMRHostDefined ? process.env.HMR_HOST.substring("https://".length) : "localhost";
+const hmrClientPort = isHMRHostDefined ? 443 : 3000;
 
 export default defineConfig({
   plugins: [svelte()],
+
   server: {
     port: 3000,
     hmr: {
-      clientPort: process.env.HMR_HOST ? 443 : 3000,
-      host: process.env.HMR_HOST
-        ? process.env.HMR_HOST.substring("https://".length)
-        : "localhost",
+      clientPort: hmrClientPort,
+      host: hmrHost
     }
   }
-})
+});
